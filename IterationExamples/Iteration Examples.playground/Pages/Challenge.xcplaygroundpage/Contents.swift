e3ad68c65ba84c574cb54863602162b245751d19
Student <student@macbook-pro-1174-corley.local>
2017-02-09 10:50:23 -0500

    Three of the four line groups

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -16,21 +16,23 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 //for value in 1...4{
-    
+
 //}
 
 for x in stride(from: 0, through: 300, by: 20){
     x
     canvas.drawLine(fromX: x, fromY: 0, toX: 300, toY: x, lineWidth: 1)
 }
-    for x in stride(from: 0, through: 300, by: 20){
-        x
-        canvas.drawLine(fromX: x, fromY: 300, toX: 0, toY: x, lineWidth: 1)
-}
 for x in stride(from: 0, through: 300, by: 20){
     x
-    canvas.drawLine(fromX: 300, fromY: x, toX: x, toY: 0, lineWidth: 1)
+    canvas.drawLine(fromX: x, fromY: 300, toX: 0, toY: x, lineWidth: 1)
 }
+//for y in stride(from: 0, through: 300, by: 20){
+for x in stride(from: 300, through: 0, by: -20){
+    
+    canvas.drawLine(fromX: 300-x, fromY: 0, toX: 0, toY: x, lineWidth: 1)
+}
+//}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
